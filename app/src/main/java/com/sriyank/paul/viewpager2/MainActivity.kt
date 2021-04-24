62cfc55e06840fe26b3ae3cac0de236fad5e6f9d
Paul-wiseman <paulndibe92@gmail.com>
2021-04-24 14:00:34 +0100

    added a simple spinner and a custom spinner

--- app/src/main/java/com/sriyank/paul/viewpager2/MainActivity.kt
+++ app/src/main/java/com/sriyank/paul/viewpager2/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.sriyank.paul.viewpager2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
+import com.sriyank.viewpager.Countries
 import com.sriyank.viewpager.Item
 import com.sriyank.viewpager.Items
 import com.sriyank.viewpager.MyAdapter
@@ -20,8 +25,32 @@ class MainActivity : AppCompatActivity() {
         /*function call to create viewpager with fragments*/
         setupViewPagerWithFragment()
         Toast.makeText(this, "testing", Toast.LENGTH_SHORT).show()
+
+        setupSimpleSpinner()
+        setupCustomSpinner()
     }
 
+    fun setupCustomSpinner(){
+        val adapter = CountryArrayAdapter(this, Countries.list!!)
+        findViewById<Spinner>(R.id.customSpinner).adapter = adapter
+    }
+    fun setupSimpleSpinner(){
+        val  adapter = ArrayAdapter.createFromResource(this, R.array.countries, android.R.layout.simple_spinner_item)
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        findViewById<Spinner>(R.id.simpleSpinner).adapter = adapter
+        findViewById<Spinner>(R.id.simpleSpinner).onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedItem = parent!!.getItemAtPosition(position)
+                Toast.makeText(this@MainActivity, "Selected", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
     fun setupViewPagerWithFragment(){
         val adapter = MyFragmentStateAdapter(this, FooItems.list!!)
         findViewById<ViewPager2>(R.id.viewPagerWithFragment).adapter =adapter
